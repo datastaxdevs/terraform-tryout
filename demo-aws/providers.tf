8e1c312516c6310c5a6de2de8eef4c27f876f2d8
Cedrick Lunven <cedrick.lunven@gmail.com>
2024-07-17 18:06:03 +0200

    OK

--- demo-aws/providers.tf
+++ demo-aws/providers.tf
@@ -2,15 +2,7 @@ provider "astra" {
   token = var.astra_token
 }
 
-provider "google" {
-  region = "us-west-2"
-}
-
 provider "aws" {
   region  = "us-west-2"
   profile = var.aws_profile
-}
-
-provider "azurerm" {
-  features {}
 }
